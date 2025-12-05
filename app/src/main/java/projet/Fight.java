93f992a19c82b29fd4d881ece780e8b6d7d53df6
vnathael <nathael.vigne@ynov.com>
2025-12-05 09:41:47 +0100

    Diff ratio deleted

--- app/src/main/java/projet/Fight.java
+++ app/src/main/java/projet/Fight.java
@@ -6,17 +6,12 @@ public class Fight {
     public void attack(Champion attacker, Champion defender) {
         Weapon attackerWeapon = attacker.getWeapon();
         Weapon defenderWeapon = defender.getWeapon();
-        int diffRatio = (defender.getMoveSpeed() + attacker.getMoveSpeed()) / 2;
         
         if (attacker.getMoveSpeed() < defender.getMoveSpeed()) {
-            for (int i = 0; i < diffRatio; i++) {
-                attacker.takeDamage(defenderWeapon);
-            }
+            attacker.takeDamage(defenderWeapon);
             defender.takeDamage(attackerWeapon);
         } else {
-            for (int i = 0; i < diffRatio; i++) {
-                defender.takeDamage(attackerWeapon);
-            }
+            defender.takeDamage(attackerWeapon);
             attacker.takeDamage(defenderWeapon);
         }
     }
