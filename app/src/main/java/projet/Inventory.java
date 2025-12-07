e3cb08d450b0e34ab69e705937e01c36c3e58af2
Nosha <95776978+noshagit@users.noreply.github.com>
2025-12-07 17:17:08 +0100

    add function for weapons & armors

--- app/src/main/java/projet/Inventory.java
+++ app/src/main/java/projet/Inventory.java
@@ -8,20 +8,48 @@ import java.util.ArrayList;
 
 public class Inventory {
     private final List<Potion> potions;
+    private final List<Potion> weapons;
+    private final List<Potion> armors;
 
     public Inventory() {
         this.potions = new ArrayList<Potion>();
+        this.weapons = new ArrayList<Potion>();
+        this.armors = new ArrayList<Potion>();
     }
 
     public void addPotion(Potion potion) {
         this.potions.add(potion);
     }
 
+    public void addWeapon(Potion weapon) {
+        this.weapons.add(weapon);
+    }
+
+    public void addArmor(Potion armor) {
+        this.armors.add(armor);
+    }
+
     public List<Potion> getPotions() {
         return potions;
     }
 
+    public List<Potion> getWeapons() {
+        return weapons;
+    }
+
+    public List<Potion> getArmors() {
+        return armors;
+    }
+
     public void removePotion(Potion potion) {
         this.potions.remove(potion);
+    }
+
+    public void removeWeapon(Potion weapon) {
+        this.weapons.remove(weapon);
+    }
+
+    public void removeArmor(Potion armor) {
+        this.armors.remove(armor);
     }
 }
